8ed6303f10c0efae2c80778d505e70b5251083eb
Luna <lunacristofanelli@gmail.com>
2023-08-28 21:12:19 -0300

    Listo diagrama kiosco

--- kiosco.ts
+++ kiosco.ts
@@ -1,4 +1,4 @@
-class Item {
+class Producto {
     protected TipoDeProducto: string;
     protected PrecioCosto: number;
   
@@ -16,31 +16,31 @@ class Item {
     }
   }
   
-  class Stock extends Item {
-    itemsDisponibles: Item[] = [];
-    itemsVendidos: Item[] = [];
+  class Stock extends Producto {
+    productosDisponibles: Producto[] = [];
+    productosVendidos: Producto[] = [];
   
     constructor(nombreArticulo: string, alCosto: number) {
       super(nombreArticulo, alCosto);
     }
   
-    public cargarItems(items: Item[]) {
-      this.itemsDisponibles.push(...items);
+    public cargarItems(productos: Producto[]) {
+      this.productosDisponibles.push(...productos);
     }
   
-    public venderItem(nombreArticulo: string) {
+    public venderProducto(nombreArticulo: string) {
       let index = -1;
   
-      for (let i = 0; i < this.itemsDisponibles.length; i++) {
-        if (this.itemsDisponibles[i].getTipoDeProducto() === nombreArticulo) {
+      for (let i = 0; i < this.productosDisponibles.length; i++) {
+        if (this.productosDisponibles[i].getTipoDeProducto() === nombreArticulo) {
           index = i;
           break; 
         }
       }
   
       if (index !== -1) {
-        const vendido = this.itemsDisponibles.splice(index, 1)[0];
-        this.itemsVendidos.push(vendido);
+        let vendido = this.productosDisponibles.splice(index, 1)[0];
+        this.productosVendidos.push(vendido);
         console.log(`Se ha vendido ${vendido.getTipoDeProducto()} por $${vendido.getPrecioCosto()}`);
       } else {
         console.log(`El artículo ${nombreArticulo} no está disponible para la venta`);
@@ -49,8 +49,8 @@ class Item {
   }
   
   let stockKiosco = new Stock("Yerba Playadito", 800);
-  stockKiosco.cargarItems([new Item("Alfajor Fantoche", 150), new Item("Gaseosa Coca Cola", 310), new Item("Chicles Bobaloo", 105)]);
+  stockKiosco.cargarItems([new Producto("Alfajor Fantoche", 150), new Producto("Gaseosa Coca Cola", 310), new Producto("Chicles Bobaloo", 105)]);
   
-  stockKiosco.venderItem("Alfajor Fantoche");
-  stockKiosco.venderItem("Gaseosa Coca Cola");
-  stockKiosco.venderItem("Yerba Playadito");
+  stockKiosco.venderProducto("Alfajor Fantoche");
+  stockKiosco.venderProducto("Gaseosa Coca Cola");
+  stockKiosco.venderProducto("Yerba Playadito");
